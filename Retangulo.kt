d549d664243d21fa9a965ecc9b85d101f540f878
thiagokkl <166646888+thiagokkl@users.noreply.github.com>
2024-05-13 16:50:48 -0300

    Create Retangulo.kt

---
+++ Retangulo.kt
@@ -0,0 +1,7 @@
+class Retangulo(var base: Double, var altura: Double){
+    fun main(){      
+        var retangulo = Retangulo(12.0, 11.0)
+        println("área: $base * $altura /n perimetro: ($base*2 + $base*2)")
+    	
+   }
+  }
