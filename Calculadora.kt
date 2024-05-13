653747725b7453bfabd33db0842c8c877c218c09
thiagokkl <166646888+thiagokkl@users.noreply.github.com>
2024-05-13 16:45:40 -0300

    Create Calculadora.kt

---
+++ Calculadora.kt
@@ -0,0 +1,18 @@
+class Calculadora(){
+    
+    fun somar(a: Int, b: Int): Int{
+        return a + b
+    }
+    
+    fun subtrair(a: Int, b: Int): Int{
+        return a - b
+    }
+
+	fun multiplicar(a: Int, b: Int): Int {
+        return a * b
+    }
+	
+	fun dividir (a: Int, b: Int): Int {
+        return a / b
+    }
+}
