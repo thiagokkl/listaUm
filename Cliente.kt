7db162d1c18bdca9e062b16942efddc787403088
thiagokkl <166646888+thiagokkl@users.noreply.github.com>
2024-05-13 18:05:21 -0300

    Create Cliente.kt

---
+++ Cliente.kt
@@ -0,0 +1,6 @@
+class Cliente(val nome: String, val cpf: String, val dataNascimento: Data) {
+    }
+    
+    fun main() {
+        val cliente = Cliente("Thiago", "123.000.333-82",13032007)
+    }
